4f9cd910b7a293a5fb52a2f235f18b65394f1633
Hakri <ivan.hardikov555@gmail.com>
2021-01-29 11:27:42 +0300

    finish

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from Ui import Ui_MainWindow
+from random import randrange
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.run)
+        self.flag = False
+
+    def run(self):
+        self.flag = True
+        self.repaint()
+
+    def paintEvent(self, e):
+        if self.flag:
+            self.flag = False
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor('yellow'))
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        x = randrange(0, 500)
+        y = randrange(0, 400)
+        r = randrange(50, 200)
+        qp.drawEllipse(int(x), int(y), int(r), int(r))
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
